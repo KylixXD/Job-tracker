cff7c7795f220bd67b36718e1686677b3d0b2292
KylixXD <rafaelnobregadelima2809@gmail.com>
2026-03-20 13:55:45 -0400

    continuar a construção do Form

--- src/Pages/NovaVaga.jsx
+++ src/Pages/NovaVaga.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+import FormVaga from "../components/FormVaga"
+
 function NovaVaga() {
     return(
         <>
             <h1>Teste Nova Vaga</h1>
             <Link to="/">Home</Link>
+
+            <FormVaga/>
         </>
     )
 }
